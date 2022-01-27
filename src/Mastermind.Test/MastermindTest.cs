2ac7508e8619a86fe4dfae9a5d69b56fee492c5a
Jeremy Meier <jeremy.meier@bbv.ch>
2022-01-27 14:29:03 +0100

    result of first try

--- src/Mastermind.Test/MastermindTest.cs
+++ src/Mastermind.Test/MastermindTest.cs
@@ -6,11 +6,60 @@ using Xunit;
 public class MastermindTest
 {
     [Fact]
-    public void Test1()
+    public void EvaluateSingleGuess()
     {
         var mastermind = new Mastermind();
-        var result = mastermind.Do(new[] { PinColor.Yellow }, new[] { PinColor.Yellow });
+        var result = mastermind.EvaluateGuess(
+            new[] { PegColor.Yellow },
+            new[] { PegColor.Yellow });
 
         result.Should().Be((1, 0));
     }
+
+    [Fact]
+    public void EvaluateForMisplacedGuesses()
+    {
+        var mastermind = new Mastermind();
+        var secret = new[]
+        {
+            PegColor.Yellow,
+            PegColor.Red
+        };
+        var guess = new[]
+        {
+            PegColor.Red,
+            PegColor.Yellow
+        };
+
+        var result = mastermind.EvaluateGuess(
+            guess,
+            secret);
+
+        result.Should().Be((0, 2));
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var mastermind = new Mastermind();
+        var secret = new[]
+        {
+            PegColor.Yellow,
+            PegColor.Red
+        };
+        var guess = new[]
+        {
+            PegColor.Red,
+            PegColor.Yellow
+        };
+
+        var result = mastermind.EvaluateGuess(
+            guess,
+            secret);
+
+        result.Should().Be((0, 2));
+    }
+
+
+
 }
--- src/Mastermind/Mastermind.cs
+++ src/Mastermind/Mastermind.cs
@@ -1,9 +1,32 @@
 ﻿namespace CleanCode.Academy.MastermindDojo.Mastermind;
 
+using System.Linq;
+
 public class Mastermind
 {
-    public (int, int) Do(PinColor[] pinColors, PinColor[] pinColors2)
+    public (int correctGuesses, int misplacedGuesses) EvaluateGuess(
+        PegColor[] guess,
+        PegColor[] secret)
     {
-        return (1,0);
+        int correctGuesses = 0;
+        int misplacedGuesses = 0;
+
+
+        for (int i = 0; i < guess.Length; i++)
+        {
+            if (guess[i] == secret[i])
+            {
+                correctGuesses++;
+            }
+            else
+            {
+                if (secret.Contains(guess[i]))
+                {
+                    misplacedGuesses++;
+                }
+            }
+        }
+
+        return (correctGuesses, misplacedGuesses);
     }
 }
