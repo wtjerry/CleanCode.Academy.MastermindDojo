0a2646c67bf15d23a35066e0ff3dc59dc0329496
Jeremy Meier <jeremy.meier@bbv.ch>
2022-11-02 14:48:32 +0100

    first try

--- src/Mastermind.Test/MastermindTest.cs
+++ src/Mastermind.Test/MastermindTest.cs
@@ -1,5 +1,6 @@
 ﻿namespace CleanCode.Academy.MastermindDojo.Mastermind.Test;
 
+using System;
 using FluentAssertions;
 using Xunit;
 
@@ -12,5 +13,67 @@ public class MastermindTest
         var result = mastermind.Do(new[] { PinColor.Yellow }, new[] { PinColor.Yellow });
 
         result.Should().Be((1, 0));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var mastermind = new Mastermind();
+        var result = mastermind.Do(
+            new[] { PinColor.Yellow, PinColor.Red },
+            new[] { PinColor.Yellow, PinColor.Blue });
+
+        result.Should().Be((1, 0));
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var mastermind = new Mastermind();
+        var f = () => mastermind.Do(new[] { PinColor.Yellow, PinColor.Red, PinColor.Green },
+            new[] { PinColor.Yellow, PinColor.Blue });
+
+        f.Should().Throw<ArgumentOutOfRangeException>()
+            .WithMessage("Arrays do not have a matching size. (Parameter 'secret')");
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var mastermind = new Mastermind();
+        var result = mastermind.Do(new[] { PinColor.Yellow, PinColor.Red, PinColor.Green },
+            new[] { PinColor.Yellow, PinColor.Green, PinColor.Red });
+
+        result.Should().Be((1, 2));
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        var mastermind = new Mastermind();
+        var result = mastermind.Do(new[] { PinColor.Red, PinColor.Yellow, PinColor.Green },
+            new[] { PinColor.Yellow, PinColor.Green, PinColor.Red });
+
+        result.Should().Be((0, 3));
+    }
+
+    [Fact]
+    public void Test6()
+    {
+        var mastermind = new Mastermind();
+        var result = mastermind.Do(new[] { PinColor.Red, PinColor.Yellow, PinColor.Green },
+            new[] { PinColor.Red, PinColor.Yellow, PinColor.Green });
+
+        result.Should().Be((3, 0));
+    }
+
+    [Fact]
+    public void Test7()
+    {
+        var mastermind = new Mastermind();
+        var result = mastermind.Do(new[] { PinColor.Red, PinColor.Yellow, PinColor.Green },
+            new[] { PinColor.Blue, PinColor.Blue, PinColor.Blue });
+
+        result.Should().Be((0, 0));
     }
 }
--- src/Mastermind/Mastermind.cs
+++ src/Mastermind/Mastermind.cs
@@ -1,9 +1,33 @@
 ﻿namespace CleanCode.Academy.MastermindDojo.Mastermind;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 public class Mastermind
 {
-    public (int, int) Do(PinColor[] pinColors, PinColor[] pinColors2)
+    public (int, int) Do(PinColor[] secret, PinColor[] guess)
     {
-        return (1,0);
+        if (secret.Length != guess.Length)
+        {
+            throw new ArgumentOutOfRangeException(nameof(secret),"Arrays do not have a matching size.");
+        }
+
+        var correct = new List<int>();
+        var misplaced = new List<int>();
+
+        for (int i = 0; i < secret.Length; i++)
+        {
+            if (secret[i] == guess[i])
+            {
+                correct.Add((int)guess[i]);
+            }
+            else if (secret.Contains(guess[i]))
+            {
+                misplaced.Add((int)guess[i]);
+            }
+        }
+
+        return (correct.Count, misplaced.Count);
     }
 }
