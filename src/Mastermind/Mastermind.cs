57a422937b1477a0d23bf661097feac35904e5ad
Jeremy Meier <jeremy.meier@bbv.ch>
2023-04-05 12:44:50 +0200

    first try

--- src/Mastermind/Mastermind.cs
+++ src/Mastermind/Mastermind.cs
@@ -1,9 +1,53 @@
 ﻿namespace CleanCode.Academy.MastermindDojo.Mastermind;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.NetworkInformation;
+
 public class Mastermind
 {
-    public (int, int) Do(PinColor[] pinColors, PinColor[] pinColors2)
+    public (int, int) Do(
+        PinColor[] pinColors,
+        PinColor[] pinColors2)
     {
-        return (1,0);
+        var wellPlaced = 0;
+        var missPlaced = 0;
+        var foundColors = new Dictionary<int, PinColor>();
+
+        if (!pinColors.Length.Equals(pinColors2.Length))
+        {
+            throw new Exception("No valid list colors");
+        }
+
+        if (pinColors.SequenceEqual(pinColors2))
+        {
+            wellPlaced = (pinColors.Length);
+            return (wellPlaced, missPlaced);
+        }
+
+        var wellPlacedIndexes = new List<PinColor>();
+        for (int i = 0; i <= pinColors.Length; i++)
+        {
+            if (pinColors[i] == pinColors2[i])
+            {
+                foundColors.Add(i, pinColors[i]);
+                wellPlaced++;
+            }
+            // for (int j = 0; j < pinColors.Length; j++)
+            // {
+            //     if (i == j && pinColors[i].Equals(pinColors[j]))
+            //     {
+            //         wellPlaced++;
+            //     }
+            //
+            // }
+        }
+
+
+
+
+        return (wellPlaced, missPlaced);
     }
 }
