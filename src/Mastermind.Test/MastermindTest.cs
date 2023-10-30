90762899564b5ef0ba64e95c789817620328bf48
Jeremy Meier <jeremy.meier@bbv.ch>
2023-10-30 16:51:02 +0100

    first try

--- src/Mastermind.Test/MastermindTest.cs
+++ src/Mastermind.Test/MastermindTest.cs
@@ -6,11 +6,95 @@ using Xunit;
 public class MastermindTest
 {
     [Fact]
-    public void Test1()
+    public void TestEinKorrekterPlatz()
     {
         var mastermind = new Mastermind();
-        var result = mastermind.Do(new[] { PinColor.Yellow }, new[] { PinColor.Yellow });
+        var result = mastermind.Evaluate(new[] { PinColor.Yellow }, new[] { PinColor.Yellow });
 
         result.Should().Be((1, 0));
     }
+
+    [Fact]
+    public void TestZweiKorrektePlaetze()
+    {
+        var mastermind = new Mastermind();
+        var result = mastermind.Evaluate(new[] { PinColor.Yellow, PinColor.Blue }, new[] { PinColor.Yellow, PinColor.Blue });
+
+        result.Should().Be((2, 0));
+    }
+
+    [Fact]
+    public void Test2KorrektePlaetzeUndZweiKorrekteFarben()
+    {
+        var mastermind = new Mastermind();
+        var result = mastermind.Evaluate(
+            new[] { PinColor.Yellow, PinColor.Blue, PinColor.Blue, PinColor.Green },
+            new[] { PinColor.Yellow, PinColor.Blue, PinColor.Green, PinColor.Blue }
+        );
+
+        result.Should().Be((2, 2));
+    }
+
+    [Fact]
+    public void Test2KorrekteFarben()
+    {
+        var mastermind = new Mastermind();
+        var result = mastermind.Evaluate(
+            new[] { PinColor.Purple, PinColor.Purple, PinColor.Blue, PinColor.Green },
+            new[] { PinColor.Green, PinColor.Blue, PinColor.Red, PinColor.Red }
+        );
+
+        result.Should().Be((0, 2));
+    }
+
+    [Fact]
+    public void Test3KorrekteFarben()
+    {
+        var mastermind = new Mastermind();
+        var result = mastermind.Evaluate(
+            new[] { PinColor.Purple, PinColor.Purple, PinColor.Blue, PinColor.Green },
+            new[] { PinColor.Green, PinColor.Blue, PinColor.Red, PinColor.Purple }
+        );
+
+        result.Should().Be((0, 3));
+    }
+
+    [Fact]
+    public void Test3KorrekteFarben2()
+    {
+        var mastermind = new Mastermind();
+        var result = mastermind.Evaluate(
+            new[] { PinColor.Purple, PinColor.Red, PinColor.Blue, PinColor.Green },
+            new[] { PinColor.Green, PinColor.Blue, PinColor.Purple, PinColor.Purple }
+        );
+
+        result.Should().Be((0, 3));
+    }
+
+    [Fact]
+    public void Test3KorrekteFarben3()
+    {
+        var mastermind = new Mastermind();
+        var result = mastermind.Evaluate(
+            new[] { PinColor.Green, PinColor.Blue, PinColor.Purple, PinColor.Purple },
+            new[] { PinColor.Purple, PinColor.Red, PinColor.Blue, PinColor.Green }
+        );
+
+        result.Should().Be((0, 3));
+    }
+
+
+    [Fact]
+    public void TestAllesFalsch()
+    {
+        var mastermind = new Mastermind();
+        var result = mastermind.Evaluate(
+            new[] { PinColor.Purple },
+            new[] { PinColor.Green }
+        );
+
+        result.Should().Be((0, 0));
+    }
+
+
 }
--- src/Mastermind/Mastermind.cs
+++ src/Mastermind/Mastermind.cs
@@ -1,9 +1,34 @@
-﻿namespace CleanCode.Academy.MastermindDojo.Mastermind;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CleanCode.Academy.MastermindDojo.Mastermind;
 
 public class Mastermind
 {
-    public (int, int) Do(PinColor[] pinColors, PinColor[] pinColors2)
+    public (int, int) Evaluate(PinColor[] solution, PinColor[] guess)
     {
-        return (1,0);
+        var correctPlaces = 0;
+        var correctColors = 0;
+        var guessHistogram = new Dictionary<PinColor, int>();
+        var solutionHistogram = new Dictionary<PinColor, int>();
+        for (int i = 0; i < solution.Length; i++)
+        {
+            guessHistogram[guess[i]] += 1;
+            solutionHistogram[solution[i]] += 1;
+            correctPlaces += solution[i] == guess[i] ? 1 : 0;
+        }
+        foreach (PinColor color in System.Enum.GetValues(typeof(PinColor)))
+        {
+            try
+            {
+                correctColors += Math.Min(guessHistogram[color], solutionHistogram[color]);
+            }
+            catch
+            {
+            }
+        }
+
+        return (correctPlaces, correctColors - correctPlaces);
     }
 }
